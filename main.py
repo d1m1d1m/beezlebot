7eaef4eb7aa886eec70486debc9dad4fb1d070f8
d1m1d1m <d1m1tr1.d3l3tang@gmail.com>
2025-07-23 00:00:22 +0200

    ignore database and update entry file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
-from core.BeezleBot import BeezleBot
+from core.custom_bot import CustomBot
 
 # Load .env file
 load_dotenv()
@@ -9,5 +9,5 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-beezle_bot = BeezleBot('!', intents)
+beezle_bot = CustomBot('!', intents)
 beezle_bot.run(os.getenv('DISCORD_BOT_TOKEN'))
